c97186385475dcc8de90d5deeb352bc8fa15421b
kendoow <kendo_ow@mail.ru>
2021-10-09 20:49:31 +0300

    1

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const name = "Вадим",
+  age = 18,
+  gender = "Мужчина";
+
+const sayHi = (name,age, gender) => {
+    console.log(`Привет, ${name}. Тебе ${age} лет и ты ${gender}`)
+}
+sayHi(name,age,gender)
+export {};
